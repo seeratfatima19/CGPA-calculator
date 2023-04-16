a83f270a513a63ea457282ba54dca23bfaf086f6
seerat <seeratfatima991@yahoo.com>
2023-04-16 14:55:52 +0500

    added calculation screen

--- app/src/main/java/com/example/cgpacalculatorpucit/HomeActivity.java
+++ app/src/main/java/com/example/cgpacalculatorpucit/HomeActivity.java
@@ -2,9 +2,14 @@ package com.example.cgpacalculatorpucit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -14,6 +19,7 @@ public class HomeActivity extends AppCompatActivity {
 
     Spinner semesters;
     ArrayAdapter<CharSequence> adapter;
+    Button nextButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +36,17 @@ public class HomeActivity extends AppCompatActivity {
 
         String sem = semesters.getSelectedItem().toString();
         int sem_num = Integer.parseInt(sem);
+
+        View.OnClickListener onClickListener  = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nextIntent = new Intent(HomeActivity.this,CalculationScreen.class);
+                startActivity(nextIntent);
+            }
+        };
+
+        nextButton= (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(onClickListener);
 
 
 
